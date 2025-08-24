6af106b829d59d3491b1c759ea1a123ca32d8948
Rashedalfoqha <146785416+Rashedalfoqha@users.noreply.github.com>
2025-08-24 14:49:19 +0300

    update user is done

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -120,9 +120,49 @@ const deleteUser = async (req, res) => {
     });
   }
 }
+const editUser = async (req, res) => {
+  const { userId } = req.params;
+  const { first_name, last_name, email, phone_number, country, username, role_id } = req.body;
+
+  try {
+    const result = await pool.query(
+      `UPDATE Users 
+       SET 
+         first_name = COALESCE($1, first_name),
+         last_name = COALESCE($2, last_name),
+         email = COALESCE($3, email),
+         phone_number = COALESCE($4, phone_number),
+         country = COALESCE($5, country),
+         username = COALESCE($6, username),
+         role_id = COALESCE($7, role_id)
+       WHERE id=$8 AND is_deleted = FALSE
+       RETURNING *`,
+      [first_name, last_name, email?.toLowerCase(), phone_number, country, username, role_id, userId]
+    );
+
+    if(result.rows.length === 0){
+      return res.status(404).json({ success: false, message: "User not found or deleted" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      user: result.rows[0]
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Error updating user",
+      error: err.message
+    });
+  }
+};
+
+
 module.exports = { 
 register, 
 login ,
 viewUsers,
 deleteUser
+, editUser
 };
--- backend/router/user.js
+++ backend/router/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const {register, login, viewUsers, deleteUser} = require('../controller/user');
+const {register, login, viewUsers, deleteUser, editUser} = require('../controller/user');
 const authentiction= require('../middleware/authentication');
 const authorization= require('../middleware/authorization');
 const usersRouter = express.Router();
@@ -9,6 +9,7 @@ usersRouter.post('/register', register);
 usersRouter.post('/login', login);
  usersRouter.get('/view', viewUsers);
 usersRouter.delete('/delete/:id', deleteUser);
+usersRouter.put('/edit/:userId', editUser);
 
 
 module.exports = usersRouter;
