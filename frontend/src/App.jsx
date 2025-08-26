fecc83b233d02835ed805c3ff48aaa81d184a9ce
Rashedalfoqha <146785416+Rashedalfoqha@users.noreply.github.com>
2025-08-26 11:20:40 +0300

    react router is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-
-import './App.css'
+import { Route, Routes } from "react-router";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-<h1 className="text-3xl font-bold underline">
-  Hello world!
-</h1>
-    </>
-  )
+    <Routes>
+      {" "}
+      <>
+        <Route
+          path="/"
+          element={
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          }
+        />
+      </>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
