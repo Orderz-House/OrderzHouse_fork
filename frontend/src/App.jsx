aeb07cda286603d45cb809ff68085a91c98a5f9f
Rashedalfoqha <146785416+Rashedalfoqha@users.noreply.github.com>
2025-08-26 14:32:06 +0300

    NavBar and Footer desgn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Counter from "./counter/Counter";
+import Navbar from "./navbar/Nav";
+import EnhancedFooter from "./footer/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/test" element={<Counter />} />
-      <Route
-        path="/"
-        element={
-          <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        }
-      />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/test" element={<Counter />} />
+        <Route
+          path="/"
+          element={
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          }
+        />
+      </Routes>
+      <EnhancedFooter />
+    </>
   );
 }
 
