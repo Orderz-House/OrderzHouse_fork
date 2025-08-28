b9cd8d32b1c947e3abf9fcd27b3e7078f0e9a62a
Rashedalfoqha <146785416+Rashedalfoqha@users.noreply.github.com>
2025-08-28 09:57:03 +0300

    new backend file

--- backend/router/subscriptions.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { subscriptionToPlan , getSubscriptionByUserId, } = require('../controller/subscriptions');
-const subscriptionsRouter = express.Router();
-
-subscriptionsRouter.post('/subscribe', subscriptionToPlan);
-subscriptionsRouter.get('/user/:userId', getSubscriptionByUserId);
-
-
-module.exports = subscriptionsRouter;
